7865c03d7f66db84a313ffa0511384b4af778b29
Zachary Hunter <hun24038@byui.edu>
2025-04-30 18:52:47 -0400

    Finished asignment three with both stretch goals.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        do
+        {
+           Random randomGenerator = new Random();
+           int randomNum = randomGenerator.Next(1, 100);
+           int guesses = 0;
+           int guess;
+           do
+           {
+             Console.Write("What is your guess? ");
+             string answer = Console.ReadLine().Trim();
+             guess = int.Parse(answer); 
+             guesses++;
+             if (guess < randomNum)
+                Console.WriteLine("Higher");
+             else if (guess > randomNum)
+                Console.WriteLine("Lower ");
+   
+           } while (guess != randomNum);
+           Console.WriteLine("You guessed it! ");
+           Console.WriteLine($"You took {guesses} guesses. ");
+           Console.Write("Would you like to play again? ");
+           playAgain = Console.ReadLine().Trim();
+        } while (playAgain == "yes");
+
     }
 }
